694c9c3c16a850326493e6433bb2bc36a83ae77d
mithun30052001 <lmithun01@gmail.com>
2024-07-11 20:33:34 +0530

    Database modified from sqllite to postgresql

--- server/prisma/migrations/20240710091401_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "PokemonType_pokemonId_idx" ON "PokemonType"("pokemonId");
-
--- CreateIndex
-CREATE INDEX "PokemonType_typeId_idx" ON "PokemonType"("typeId");
-
--- CreateIndex
-CREATE INDEX "pokemon_type_index" ON "PokemonType"("pokemonId", "typeId");
